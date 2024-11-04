ef4ab323f5cae6ab29c64a1c4dba15d7621900d3
diamondburned <diamond@libdb.so>
2024-11-03 22:31:08 -0800

    Add root user to dev pgSQL config

--- nix/modules/e2clicker-postgresql/default.nix
+++ nix/modules/e2clicker-postgresql/default.nix
@@ -14,10 +14,19 @@
         name = "e2clicker-backend";
         ensureDBOwnership = true;
       }
+      {
+        name = "root";
+        ensureClauses = {
+          login = true;
+          superuser = true;
+        };
+      }
     ];
     identMap = ''
       e2clicker-backend e2clicker-backend e2clicker-backend
     '';
     extraPlugins = ps: with ps; [ ];
   };
+
+  environment.systemPackages = with pkgs; [ pgcli ];
 }
