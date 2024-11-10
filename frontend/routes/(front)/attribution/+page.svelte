233871d9dec07bf71d9c7fb4456f4891142698ec
diamondburned <diamond@libdb.so>
2024-11-10 00:38:26 -0800

    Add /attribution page

---
+++ frontend/routes/(front)/attribution/+page.svelte
@@ -0,0 +1,55 @@
+<main class="container spaced-2">
+  <h1>Attribution</h1>
+
+  <section>
+    <h2>Special Thanks</h2>
+    <p>
+      The predictive algorithms in this project would not have existed without the numerous works
+      from the community. Special thanks to the following for their contributions:
+    </p>
+    <ul>
+      <li>
+        <a href="https://github.com/a1ix2">alix</a> for the original estrannai.se project and its findings
+        on the pharmacokinetics of estradiol.
+      </li>
+      <li>
+        <a href="https://transfemscience.org">Transfem Science</a> for their original work on estradiol
+        pharmacokinetics, which inspired the estrannai.se project.
+      </li>
+    </ul>
+  </section>
+
+  <section>
+    <h2>Awesome Open-source Libraries</h2>
+    <p>This project would not have been possible without the following open-source libraries:</p>
+    <ul>
+      <li>
+        <a href="https://estrannai.se">estrannai.se</a> for the open-source estradiol pharmacokinetics
+        algorithms.
+      </li>
+      <li>
+        <a href="https://go.dev">Go</a> and <a href="https://svelte.dev">Svelte</a> for the awesome language
+        and framework, which built the foundation of this project.
+      </li>
+      <li>
+        <a href="https://www.tradingview.com/">Trading View</a> for the awesome charts library.
+      </li>
+    </ul>
+  </section>
+
+  <section>
+    <h2>Source Code</h2>
+    <a class="mx-auto" href="https://github.com/diamondburned/e2clicker" role="button">
+      Available on <b>GitHub</b>
+    </a>
+  </section>
+
+  <section>
+    <h2>License</h2>
+    <p>
+      This project is licensed under the GNU General Public License v3.0. You can find the full
+      license at <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">gnu.org</a> or in the
+      project's source code at <code>LICENSE.md</code>.
+    </p>
+  </section>
+</main>
