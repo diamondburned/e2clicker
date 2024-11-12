e6fa8e5e60e9a46c92a67748181278011fd86b21
diamondburned <diamond@libdb.so>
2024-11-12 02:39:44 -0800

    Even more optimizations

--- frontend/lib/components/Icon.svelte
+++ frontend/lib/components/Icon.svelte
@@ -6,11 +6,6 @@
 
 <style lang="scss">
   .material-symbols-rounded {
-    font-variation-settings:
-      "FILL" 1,
-      "wght" 400,
-      "GRAD" 0,
-      "opsz" 24;
     vertical-align: text-bottom;
   }
 </style>
